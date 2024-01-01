6c6bd673ffd8867125c94eff280e2644a564f892
chunhui_true <chunhui_true@163.com>
2024-01-01 19:55:14 +0800

    x

--- src/pktstrm.rs
+++ src/pktstrm.rs
@@ -1,18 +1,21 @@
+use core::cmp::Ordering;
+use etherparse::TransportHeader;
 use packet::Packet;
+use std::collections::BinaryHeap;
 
-#[derive(Debug, Clone, Copy)]
-pub struct PktStrm {
-    // 一个堆排序。或者一个vec
-    // a: u16,
+#[derive(Debug, Clone)]
+pub struct PktStrm<'a> {
+    pkt_cache: BinaryHeap<SeqPacket<'a>>,
 }
 
-impl PktStrm {
+impl<'a> PktStrm<'a> {
     pub fn new() -> Self {
-        PktStrm {}
+        PktStrm { pkt_cache: BinaryHeap::new() }
     }
 
     /// 数据包处理，放入缓存
     pub fn put(&mut self, _pkt: &Packet) {
+        // 需要判断是否是tcp
         todo!()
     }
 
@@ -25,8 +28,41 @@ impl PktStrm {
     }
 }
 
-impl Default for PktStrm {
+impl Default for PktStrm<'_> {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+#[derive(Debug, Clone)]
+struct SeqPacket<'a>(Packet<'a>);
+
+impl PartialEq for SeqPacket<'_> {
+    fn eq(&self, other: &Self) -> bool {
+        match (&self.0.header.borrow().as_ref().unwrap().transport, &other.0.header.borrow().as_ref().unwrap().transport) {
+            (Some(TransportHeader::Tcp(s_tcph)), Some(TransportHeader::Tcp(o_tcph))) => {
+                s_tcph.sequence_number == o_tcph.sequence_number
+            }
+            _ => false,
+        }
+    }
+}
+
+impl Eq for SeqPacket<'_> {}
+
+impl PartialOrd for SeqPacket<'_> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for SeqPacket<'_> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match (&self.0.header.borrow().as_ref().unwrap().transport, &other.0.header.borrow().as_ref().unwrap().transport) {
+            (Some(TransportHeader::Tcp(s_tcph)), Some(TransportHeader::Tcp(o_tcph))) => {
+                s_tcph.sequence_number.cmp(&o_tcph.sequence_number)
+            }
+            _ => Ordering::Equal,
+        }
     }
 }
