d507de6c9f4c16ef31eea760ae5d6c8d85b1e5ee
chunhui_true <chunhui_true@163.com>
2024-01-08 22:27:21 +0800

    x

--- src/pktstrm.rs
+++ src/pktstrm.rs
@@ -116,10 +116,42 @@ impl Ord for SeqPacket {
 
 #[cfg(test)]
 mod tests {
-    // use super::*;
+    use super::*;
+    use etherparse::*;    
 
-    // #[test]
-    // fn tt() {
+    fn make_pkt_data(seq: u32) -> Vec<u8> {
+        //setup the packet headers
+        let builder = PacketBuilder::
+        ethernet2([1,2,3,4,5,6],     //source mac
+                  [7,8,9,10,11,12]) //destionation mac
+            .ipv4([192,168,1,1], //source ip
+                  [192,168,1,2], //desitionation ip
+                  20)            //time to life
+            .tcp(21,    //source port 
+                 1234,  //desitnation port
+                 seq,     //sequence number
+                 26180) //window size
+        //set additional tcp header fields
+            .ns() //set the ns flag
+        //supported flags: ns(), fin(), syn(), rst(), psh(), ece(), cwr()
+            .ack(123) //ack flag + the ack number
+            .urg(23) //urg flag + urgent pointer
+            .options(&[
+                TcpOptionElement::Noop,
+                TcpOptionElement::MaximumSegmentSize(1234)
+            ]).unwrap();
+        //payload of the tcp packet
+        let payload = [1,2,3,4,5,6,7,8];
+        //get some memory to store the result
+        let mut result = Vec::<u8>::with_capacity(builder.size(payload.len()));
+        //serialize
+        //this will automatically set all length fields, checksums and identifiers (ethertype & protocol)
+        builder.write(&mut result, &payload).unwrap();
+        result
+    }
+    
+    #[test]
+    fn test_seqpacket_eq() {
         
-    // }
+    }
 }
