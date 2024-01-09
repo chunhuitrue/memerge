8d3ba6726b571865d03613f5cb4c765e52389a79
chunhui_true <chunhui_true@163.com>
2024-01-09 22:34:02 +0800

    x

--- src/packet.rs
+++ src/packet.rs
@@ -12,27 +12,28 @@ pub struct PktHeader {
     pub vlan: Option<VlanHeader>,
     pub ip: Option<IpHeader>,
     pub transport: Option<TransportHeader>,
-    pub payload_offset: usize
+    pub payload_offset: usize,
+    pub payload_len: usize
 }
 
 #[derive(Eq, PartialEq, Clone)]
 pub struct Packet {
     pub timestamp: u128,
-    pub data_len: u32,
     pub data: [u8; MAX_PACKET_LEN],
+    pub data_len: usize,
     pub header: RefCell<Option<PktHeader>>
 }
 
 impl Packet {
-    pub fn new(ts: u128, len: u32, data: &[u8]) -> Rc<Packet> {
+    pub fn new(ts: u128, len: usize, data: &[u8]) -> Rc<Packet> {
         let mut pkt = Packet {
             timestamp: ts,
             data_len: len,
             data: [0; MAX_PACKET_LEN],
             header: RefCell::new(None)
         };
-        let s_data = &mut pkt.data[..len.try_into().unwrap()];
-        s_data.copy_from_slice(&data[..len.try_into().unwrap()]);
+        let s_data = &mut pkt.data[..len];
+        s_data.copy_from_slice(&data[..len]);
         Rc::new(pkt)
     }
 
@@ -44,7 +45,8 @@ impl Packet {
                     vlan: headers.vlan,
                     ip: headers.ip,
                     transport: headers.transport,
-                    payload_offset: headers.payload.as_ptr() as usize - self.data.as_ptr() as usize
+                    payload_offset: headers.payload.as_ptr() as usize - self.data.as_ptr() as usize,
+                    payload_len: self.data_len - (headers.payload.as_ptr() as usize - self.data.as_ptr() as usize)
                 }));
                 Ok(())
             }
@@ -58,6 +60,10 @@ impl Packet {
         } else {
             0
         }
+    }
+
+    pub fn payload_len(&self) -> u32 {
+        self.header.borrow().as_ref().unwrap().payload_len.try_into().unwrap()
     }
 }
 
--- src/pktstrm.rs
+++ src/pktstrm.rs
@@ -20,9 +20,8 @@ impl PktStrm {
         }
     }
 
-    /// 放入缓存，准备重组
-    ///
-    pub fn put(&mut self, pkt: Rc<Packet>) {
+    // 放入缓存，准备重组
+    pub fn push(&mut self, pkt: Rc<Packet>) {
         if let Some(TransportHeader::Tcp(_)) = &pkt.header.borrow().as_ref().unwrap().transport {
             if self.cache.len() >= MAX_CACHE_PKTS {
                 return;
@@ -32,36 +31,82 @@ impl PktStrm {
         }
     }
 
-    /// 得到一个严格有序，连续seq的数据包
-    pub fn get_ord_pkt(&mut self) -> Option<Rc<Packet>> {
+    // 无论是否严格seq连续，都peek一个当前包的clone
+    pub fn peek(&self) -> Option<Rc<Packet>> {
+        self.cache.peek().map(|rev_pkt| {
+            let SeqPacket(pkt) = &rev_pkt.0;
+            pkt.clone()
+        })
+    }
+    
+    // peek一个seq严格有序的包，不严格有序就为None
+    pub fn peek_ord(&self) -> Option<Rc<Packet>> {
         if self.expect_seq == 0 {
-            if let Some(pkt) = self.cache.pop().map(|rev_pkt| rev_pkt.0.0) {
-                self.expect_seq = pkt.seq() + pkt.data_len;
-                return Some(pkt);
-            } else {
-                return None;
-            }
+           return self.peek();
         }
 
-        if let Some(pkt) = self.cache.peek().map(|rev_pkt| &rev_pkt.0.0) {
+        if let Some(pkt) = self.peek() {
             if pkt.seq() == self.expect_seq {
-                self.expect_seq += pkt.data_len;
-                return self.cache.pop().map(|rev_pkt| rev_pkt.0.0);
-            } else if pkt.seq() + pkt.data_len <= self.expect_seq {
-                self.cache.pop();
+                return self.peek();
+            } else if pkt.seq() + pkt.payload_len() <= self.expect_seq {
                 return None;
-            } else if pkt.seq() < self.expect_seq && pkt.seq() + pkt.data_len > self.expect_seq {
-                self.expect_seq =  pkt.seq() + pkt.data_len;
-                return self.cache.pop().map(|rev_pkt| rev_pkt.0.0);                
+            } else if pkt.seq() < self.expect_seq && pkt.seq() + pkt.payload_len() > self.expect_seq {
+                return self.peek();
             }
             None
         } else {
             None
         }
     }
+
+    // 判断当前包是否是seq严格有序
+    pub fn is_top_ord(&self) -> bool {
+        if self.peek_ord().is_none() {
+            return false;
+        }
+        true
+    }
     
-    /// 链接结束
-    pub fn finish(&mut self) {
+    // 无论是否严格seq连续，都pop一个当前包
+    pub fn pop(&mut self) -> Option<Rc<Packet>> {
+        self.cache.pop().map(|rev_pkt| rev_pkt.0.0)
+    }
+
+    // pop一个seq严格有序的包,如果top的不是有序，None
+    pub fn pop_ord(&mut self) -> Option<Rc<Packet>> {
+        if let Some(pkt) = self.peek_ord() {
+            if self.expect_seq == 0 {
+                self.expect_seq = pkt.seq() + pkt.payload_len();
+                return self.pop();
+            }
+
+            if pkt.seq() == self.expect_seq {
+                self.expect_seq += pkt.payload_len();
+                self.pop()
+            } else if pkt.seq() + pkt.payload_len() <= self.expect_seq {
+                self.pop();
+                return None;
+            } else if pkt.seq() < self.expect_seq && pkt.seq() + pkt.payload_len() > self.expect_seq {
+                self.expect_seq = pkt.seq() + pkt.payload_len();
+                return self.pop();                
+            } else {
+                return None;
+            }
+        } else {
+            None
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.cache.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+    
+    // 链接结束
+    pub fn clear(&mut self) {
         self.cache.clear();
     }
 
@@ -119,7 +164,185 @@ mod tests {
     use super::*;
     use etherparse::*;    
 
-    fn make_pkt_data(seq: u32) -> Vec<u8> {
+    #[test]
+    fn test_pkt() {
+        let pkt1 = make_pkt_data(123);
+        let _ = pkt1.decode();
+        dbg!(pkt1.data_len);        
+        dbg!(pkt1.header.borrow().as_ref().unwrap().payload_offset);
+        dbg!(pkt1.header.borrow().as_ref().unwrap().payload_len);
+        assert_eq!(72, pkt1.data_len);
+        assert_eq!(62, pkt1.header.borrow().as_ref().unwrap().payload_offset);
+        assert_eq!(10, pkt1.header.borrow().as_ref().unwrap().payload_len);
+    }
+    
+    #[test]
+    fn test_seqpacket_eq() {
+        let pkt1 = make_pkt_data(123);
+        let _ = pkt1.decode();
+        let pkt1 = SeqPacket(pkt1);
+        let pkt2 = make_pkt_data(123);
+        let _ = pkt2.decode();
+        let pkt2 = SeqPacket(pkt2);
+        assert_eq!(pkt1, pkt2);
+
+        let pkt1 = make_pkt_data(123);        
+        let _ = pkt1.decode();
+        let pkt1 = SeqPacket(pkt1);
+        let pkt2 = make_pkt_data(111);
+        let _ = pkt2.decode();
+        let pkt2 = SeqPacket(pkt2);
+        assert_ne!(pkt1, pkt2);
+    }
+
+    #[test]
+    fn test_seqpacket_ord() {
+        let pkt1 = make_pkt_data(123);
+        let _ = pkt1.decode();
+        let pkt1 = SeqPacket(pkt1);
+        let pkt2 = make_pkt_data(123);
+        let _ = pkt2.decode();
+        let pkt2 = SeqPacket(pkt2);
+        assert!(pkt1 == pkt2);
+
+        let pkt1 = make_pkt_data(123);
+        let _ = pkt1.decode();
+        let pkt1 = SeqPacket(pkt1);
+        let pkt2 = make_pkt_data(111);
+        let _ = pkt2.decode();
+        let pkt2 = SeqPacket(pkt2);
+        assert!(pkt1 > pkt2);
+
+        let pkt1 = make_pkt_data(123);
+        let _ = pkt1.decode();
+        let pkt1 = SeqPacket(pkt1);
+        let pkt2 = make_pkt_data(223);
+        let _ = pkt2.decode();
+        let pkt2 = SeqPacket(pkt2);
+        assert!(pkt1 < pkt2);
+    }
+
+    #[test]
+    fn test_push() {
+        let mut stm = PktStrm::new();
+        
+        let pkt1 = make_pkt_data(123);
+        let _ = pkt1.decode();
+        stm.push(pkt1);
+        assert_eq!(1, stm.len());
+        
+        let pkt2 = make_pkt_data(123);
+        let _ = pkt2.decode();
+        stm.push(pkt2);
+        assert_eq!(2, stm.len());
+    }
+
+    #[test]
+    fn test_peek() {
+        let mut stm = PktStrm::new();
+        
+        let pkt1 = make_pkt_data(1);
+        let _ = pkt1.decode();
+        stm.push(pkt1);
+
+        let pkt2 = make_pkt_data(30);
+        let _ = pkt2.decode();
+        stm.push(pkt2);
+
+        let pkt3 = make_pkt_data(80);
+        let _ = pkt3.decode();
+        stm.push(pkt3);
+        
+        assert_eq!(1, stm.peek().unwrap().seq());
+        stm.pop();
+        assert_eq!(30, stm.peek().unwrap().seq());
+        stm.pop();
+        assert_eq!(80, stm.peek().unwrap().seq());
+        stm.pop();        
+        assert!(stm.is_empty());
+        stm.clear();        
+    }
+
+    // 插入的包严格有序 1-10 11-12 
+    #[test]
+    fn test_peek_ord() {
+        let mut stm = PktStrm::new();
+        // 1 - 10
+        let seq1 = 1;
+        let pkt1 = make_pkt_data(seq1);
+        let _ = pkt1.decode();
+        // 11 - 20
+        let seq2 = seq1 + pkt1.payload_len();
+        let pkt2 = make_pkt_data(seq2);
+        let _ = pkt2.decode();
+        // 21 - 30
+        let seq3 = seq2 + pkt2.payload_len();
+        let pkt3 = make_pkt_data(seq3);
+        let _ = pkt3.decode();
+
+        stm.push(pkt2.clone());
+        stm.push(pkt3);
+        stm.push(pkt1.clone());
+        
+        assert_eq!(seq1, stm.peek_ord().unwrap().seq());
+        assert_eq!(seq1, stm.pop_ord().unwrap().seq());
+        assert_eq!(seq2, stm.peek_ord().unwrap().seq());
+        assert_eq!(seq2, stm.pop_ord().unwrap().seq());
+        assert_eq!(seq3, stm.peek_ord().unwrap().seq());
+        assert_eq!(seq3, stm.pop_ord().unwrap().seq());
+        assert!(stm.is_empty());
+        stm.clear();
+    }
+
+    // 插入的包有重传 1-10 11-20 1-10 21-30
+    #[test]
+    fn test_peek_ord_retrans() {
+        let mut stm = PktStrm::new();
+        // 1 - 10
+        let seq1 = 1;
+        let pkt1 = make_pkt_data(seq1);
+        let _ = pkt1.decode();
+        // 11 - 20
+        let seq2 = seq1 + pkt1.payload_len();
+        let pkt2 = make_pkt_data(seq2);
+        let _ = pkt2.decode();
+        // 21 - 30
+        let seq3 = seq2 + pkt2.payload_len();
+        let pkt3 = make_pkt_data(seq3);
+        let _ = pkt3.decode();
+
+        stm.push(pkt1.clone());        
+        stm.push(pkt2.clone());
+        stm.push(pkt1.clone());
+        stm.push(pkt3);
+
+        dbg!("pkt1 payload len", pkt1.payload_len());        
+        dbg!("seq1", seq1);
+        dbg!("seq2", seq2);
+        dbg!("seq3", seq3);
+        
+        assert_eq!(seq1, stm.peek_ord().unwrap().seq()); // 看到pkt1
+        assert_eq!(seq1, stm.pop_ord().unwrap().seq());  // 弹出pkt1
+        assert_eq!(3, stm.len());
+        assert_eq!(None, stm.peek_ord());                // 重复的pkt1是top，返回none
+        assert_eq!(None, stm.pop_ord());                 // 重复的pkt1是top，释放了这个pkt1，返回none
+
+        dbg!(stm.len());
+        dbg!(stm.expect_seq);
+        assert_eq!(2, stm.len());
+        
+        // dbg!(stm.len());
+        // dbg!(stm.peek().unwrap().seq());
+        // assert_eq!(seq2, stm.peek_ord().unwrap().seq())  // pkt2是top
+    }
+
+    // 插入的包有覆盖重传 1-10 11-20 15-25 25-35
+    #[test]
+    fn test_peek_ord_cover() {
+
+    }
+    
+    fn make_pkt_data(seq: u32) -> Rc<Packet> {
         //setup the packet headers
         let builder = PacketBuilder::
         ethernet2([1,2,3,4,5,6],     //source mac
@@ -127,10 +350,10 @@ mod tests {
             .ipv4([192,168,1,1], //source ip
                   [192,168,1,2], //desitionation ip
                   20)            //time to life
-            .tcp(21,    //source port 
-                 1234,  //desitnation port
+            .tcp(25,    //source port 
+                 4000,  //desitnation port
                  seq,     //sequence number
-                 26180) //window size
+                 1024) //window size
         //set additional tcp header fields
             .ns() //set the ns flag
         //supported flags: ns(), fin(), syn(), rst(), psh(), ece(), cwr()
@@ -141,17 +364,14 @@ mod tests {
                 TcpOptionElement::MaximumSegmentSize(1234)
             ]).unwrap();
         //payload of the tcp packet
-        let payload = [1,2,3,4,5,6,7,8];
+        let payload = [1,2,3,4,5,6,7,8,9,10];
         //get some memory to store the result
         let mut result = Vec::<u8>::with_capacity(builder.size(payload.len()));
         //serialize
         //this will automatically set all length fields, checksums and identifiers (ethertype & protocol)
         builder.write(&mut result, &payload).unwrap();
-        result
-    }
-    
-    #[test]
-    fn test_seqpacket_eq() {
+        // println!("result len:{}", result.len());
         
+        Packet::new(1, result.len().try_into().unwrap(), &result)
     }
 }
