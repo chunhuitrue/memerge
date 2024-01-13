b6f28a7beea58b8f329808e6bd5952dc337a1b64
chunhui_true <chunhui_true@163.com>
2024-01-13 16:12:04 +0800

    x

--- src/util.rs
+++ src/util.rs
@@ -36,82 +36,74 @@ mod tests {
     use super::*;
 
     #[test]
+    #[cfg(target_endian = "little")]
     fn test_ntohl_little_endian() {
-        if cfg!(target_endian = "little") {        
-            let value: u32 = 0x12345678; // 网络
-            let expected: u32 = 0x78563412; // 主机
-            let converted_value = ntohl(value);
-            assert_eq!(converted_value, expected);
-        }
+        let value: u32 = 0x12345678; // 网络
+        let expected: u32 = 0x78563412; // 主机
+        let converted_value = ntohl(value);
+        assert_eq!(converted_value, expected);
     }
 
     #[test]
+    #[cfg(target_endian = "big")]    
     fn test_ntohl_big_endian() {
-        if cfg!(target_endian = "big") {
-            let value: u32 = 0x12345678;
-            let expected: u32 = 0x12345678;
-            let converted_value = ntohl(value);
-            assert_eq!(converted_value, expected);
-        }
+        let value: u32 = 0x12345678;
+        let expected: u32 = 0x12345678;
+        let converted_value = ntohl(value);
+        assert_eq!(converted_value, expected);
     }    
     
     #[test]
+    #[cfg(target_endian = "little")]
     fn test_ntohs_little_endian() {
-        if cfg!(target_endian = "little") {
-            let value: u16 = 0x1234; // 网络
-            let expected: u16 = 0x3412; // 主机
-            let converted_value = ntohs(value);
-            assert_eq!(converted_value, expected);
-        }
+        let value: u16 = 0x1234; // 网络
+        let expected: u16 = 0x3412; // 主机
+        let converted_value = ntohs(value);
+        assert_eq!(converted_value, expected);
     }
 
     #[test]
+    #[cfg(target_endian = "big")]
     fn test_ntohs_big_endian() {
-        if cfg!(target_endian = "big") {
-            let value: u16 = 0x1234; // 网络
-            let expected: u16 = 0x1234; // 主机
-            let converted_value = ntohs(value);
-            assert_eq!(converted_value, expected);
-        }
+        let value: u16 = 0x1234; // 网络
+        let expected: u16 = 0x1234; // 主机
+        let converted_value = ntohs(value);
+        assert_eq!(converted_value, expected);
     }
 
     #[test]
+    #[cfg(target_endian = "little")]    
     fn test_htonl_little_endian() {
-        if cfg!(target_endian = "little") {
-            let value: u32 = 0x12345678;
-            let expected: u32 = 0x78563412;
-            let converted_value = htonl(value);
-            assert_eq!(converted_value, expected);
-        }
+        let value: u32 = 0x12345678;
+        let expected: u32 = 0x78563412;
+        let converted_value = htonl(value);
+        assert_eq!(converted_value, expected);
     }
     
     #[test]
+    #[cfg(target_endian = "big")]    
     fn test_htonl_big_endian() {
-        if cfg!(target_endian = "big") {
-            let value: u32 = 0x12345678;
-            let expected: u32 = 0x12345678;
-            let converted_value = htonl(value);
-            assert_eq!(converted_value, expected);
-        }
+        let value: u32 = 0x12345678;
+        let expected: u32 = 0x12345678;
+        let converted_value = htonl(value);
+        assert_eq!(converted_value, expected);
     }
 
     #[test]
+    #[cfg(target_endian = "little")]    
     fn test_htons_little_endian() {
-        if cfg!(target_endian = "little") {
-            let value: u16 = 0x1234;
-            let expected: u16 = 0x3412;
-            let converted_value = htons(value);
-            assert_eq!(converted_value, expected);
-        }
+        let value: u16 = 0x1234;
+        let expected: u16 = 0x3412;
+        let converted_value = htons(value);
+        assert_eq!(converted_value, expected);
     }
 
     #[test]
+    #[cfg(target_endian = "big")]    
     fn test_htons_big_endian() {
-        if cfg!(target_endian = "big") {
-            let value: u16 = 0x1234;
-            let expected: u16 = 0x1234;
-            let converted_value = htons(value);
-            assert_eq!(converted_value, expected);
-        }
+        let value: u16 = 0x1234;
+        let expected: u16 = 0x1234;
+        let converted_value = htons(value);
+        assert_eq!(converted_value, expected);
     }
 }
